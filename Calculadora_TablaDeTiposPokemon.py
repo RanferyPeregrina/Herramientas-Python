8497bf1f9e07fff54f80dc5593440d9d4a68a144
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2024-06-06 06:22:28 +0200

    Añadididos Tipos
    Añadidos tipos Fuego Agua Bicho y Dragón para la tabla de tipos.
    A medias los eléctricos.

--- Calculadora_TablaDeTiposPokemon.py
+++ Calculadora_TablaDeTiposPokemon.py
@@ -27,8 +27,131 @@ Tipos ={
 }
 
 def CambiarFuego(Tipos):
+    #Debil para atacar
     Tipos["Agua"] = -1
+    Tipos["Dragon"] = -1
+    Tipos["Fuego"] = -1
+    Tipos["Roca"] = -1
+    #Debil para resistir
+    Tipos["Agua"] = -1
+    Tipos["Roca"] = -1
+    Tipos["Tierra"] = -1
+
+    #Fuerte para atacar
+    Tipos["Acero"] = +1
+    Tipos["Bicho"] = +1
+    Tipos["Hielo"] = +1
+    Tipos["Planta"] = +1
+    #Fuerte para resistir
+    Tipos["Acero"] = +1
+    Tipos["Fuego"] = +1
+    Tipos["Bicho"] = +1
+    Tipos["Hada"] = +1
+    Tipos["Hielo"] = +1
+    Tipos["Planta"] = +1
     
+def CambiarAcero(Tipos):
+    #Debil para atacar
+    Tipos["Acero"] = -1
+    Tipos["Agua"] = -1
+    Tipos["Fuego"] = -1
+    Tipos["Electrico"] = -1
+    #Debil para resistir
+    Tipos["Fuego"] = -1
+    Tipos["Lucha"] = -1
+    Tipos["Tierra"] = -1
+
+    #Fuerte para atacar
+    Tipos["Hada"] = +1
+    Tipos["Roca"] = +1
+    Tipos["Hielo"] = +1
+    #Fuerte para resistir
+    Tipos["Acero"] = +1
+    Tipos["Dragon"] = +1
+    Tipos["Bicho"] = +1
+    Tipos["Hada"] = +1
+    Tipos["Hielo"] = +1
+    Tipos["Normal"] = +1
+    Tipos["Planta"] = +1
+    Tipos["Psiquico"] = +1
+    Tipos["Roca"] = +1
+
+def CambiarAgua(Tipos):
+    #Debil para atacar
+    Tipos["Dragon"] = -1
+    Tipos["Agua"] = -1
+    Tipos["Planta"] = -1
+    #Debil para resistir
+    Tipos["Electrico"] = -1
+    Tipos["Planta"] = -1
+
+    #Fuerte para atacar
+    Tipos["Fuego"] = +1
+    Tipos["Roca"] = +1
+    Tipos["Tierra"] = +1
+    #Fuerte para resistir
+    Tipos["Acero"] = +1
+    Tipos["Agua"] = +1
+    Tipos["Fuego"] = +1
+    Tipos["Hielo"] = +1
+
+def CambiarBicho(Tipos):
+    #Debil para atacar
+    Tipos["Acero"] = -1
+    Tipos["Agua"] = -1
+    Tipos["Fantasma"] = -1
+    Tipos["Fuego"] = -1
+    Tipos["Lucha"] = -1
+    Tipos["Veneno"] = -1
+    Tipos["Volador"] = -1
+    #Debil para resistir
+    Tipos["Fuego"] = -1
+    Tipos["Roca"] = -1
+
+    #Fuerte para atacar
+    Tipos["Planta"] = +1
+    Tipos["Psiquico"] = +1
+    Tipos["Siniestro"] = +1
+    #Fuerte para resistir
+    Tipos["Planta"] = +1
+    Tipos["Lucha"] = +1
+    Tipos["Tierra"] = +1
+
+def CambiarDragon(Tipos):
+    #Debil para atacar
+    Tipos["Acero"] = -1
+    Tipos["Hada"] = -1
+    #Debil para resistir
+    Tipos["Dragon"] = -1
+    Tipos["Hada"] = -1
+    Tipos["Hielo"] = -1
+
+    #Fuerte para atacar
+    Tipos["Dragon"] = +1
+    #Fuerte para resistir
+    Tipos["Planta"] = +1
+    Tipos["Agua"] = +1
+    Tipos["Electrico"] = +1
+    Tipos["Fuego"] = +1
+
+def CambiarElectrico(Tipos):
+    #Debil para atacar
+    Tipos["Electrico"] = -1
+    Tipos["Dragon"] = -1
+    Tipos["Planta"] = -1
+    Tipos["Tierra"] = -1
+    #Debil para resistir
+    Tipos["Tierra"] = -1
+
+
+    #Fuerte para atacar
+    Tipos["Planta"] = +1
+    Tipos["Psiquico"] = +1
+    Tipos["Siniestro"] = +1
+    #Fuerte para resistir
+    Tipos["Planta"] = +1
+    Tipos["Lucha"] = +1
+    Tipos["Tierra"] = +1
 
 
 print("= "*20)
