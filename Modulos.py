1c35bef4a127b9071691c2fd0e824b85c928cf15
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2025-10-04 13:46:18 -0600

    Reorganización primaria de archivos
    Falta separar los que son de matemáticas

--- Modulos.py
+++
@@ -1,7 +0,0 @@
-Modulo = 255
-while True:
-    print()
-    Divisor = int(input("Ingrese el número que quiere como divisor:  "))
-
-    Respuesta = Divisor % Modulo
-    print(f"Resultado: {Respuesta}")
